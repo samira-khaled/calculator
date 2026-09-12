c4eda4cdb59435b557bc217301b4f34238c1ad65
samira khaled <samerakhaled8739@gmail.com>
2026-09-12 22:12:33 +0300

    feat: implement basic calculator operations

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string start;
+            do {
+                Console.Write("Enter the first number: ");
+                double X = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter the second number: ");
+                double Y = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter an operator (+, -, *, /): ");
+                string Operator = Console.ReadLine();
+                if (Operator == "+")
+                {
+                    double Result = X + Y;
+                    Console.WriteLine(Result);
+                }
+                else if (Operator == "-")
+                {
+                    double Result = X - Y;
+                    Console.WriteLine(Result);
+                }
+                else if (Operator == "*")
+                {
+                    double Result = X * Y;
+                    Console.WriteLine(Result);
+                }
+                else if (Operator == "/")
+                {
+                    double Result = X / Y;
+                    Console.WriteLine(Result);
+                }
+                Console.Write("do caculate again read y/n : ");
+               start= Console.ReadLine();
+            } while (start=="y");
+            
+
         }
     }
 }
